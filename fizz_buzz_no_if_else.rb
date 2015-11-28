c0ec428e3abbed74961ec666d484030f32c2bbea
Regis <boudinot.regis@yahoo.com>
2015-11-28 00:02:01 -0700

    Bored, refactored fizzbuzz - no if-elsif-else - hashes are awesome

---
+++ fizz_buzz_no_if_else.rb
@@ -0,0 +1,10 @@
+def fizzbuzz(n)
+	{
+	  'Fizz'     => (n % 3 == 0) && (n % 5 != 0),
+	  'Buzz'     => (n % 3 != 0) && (n % 5 == 0),
+	  'FizzBuzz' => (n % 5 == 0) && (n % 5 == 0),
+	  "#{n}"     => (n % 3 != 0) && (n % 5 != 0)
+  }.key(true)
+end
+
+1.upto(100).each { |num| puts fizzbuzz(num) }
